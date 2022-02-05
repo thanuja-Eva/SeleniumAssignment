22a9f21e4aaf43f88f9b63d560e3f8c4f8e6d287
thanuja-Eva <99018245+thanuja-Eva@users.noreply.github.com>
2022-02-05 08:58:53 +0530

    Add files via upload
    Day2 assignment

---
+++ Area.java
@@ -0,0 +1,18 @@
+package com.HA.Assignment.day2;
+
+public class Area {
+	
+
+	public static double areaOfSquare(int side) {
+		double area=side*side;
+		return area;
+		
+	}
+	
+	public  static double areaOfTraphezium(double a, double b,double height) {
+		double trapheziumArea=((a+b)*height)/2;
+		return trapheziumArea;
+	}
+	
+
+}
---
+++ AreaTest.java
@@ -0,0 +1,19 @@
+package com.HA.Assignment.day2;
+
+
+
+public class AreaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Area a=new Area();
+	
+	double result=Area.areaOfSquare(5);
+	System.out.println(result);
+	
+	double result1=Area.areaOfTraphezium(2, 5, 2);
+	System.out.println(result1);
+
+	}
+
+}
